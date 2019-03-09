bb31e4c2286ead3f766bff61a493c582c5d57ae9
Renu Bansal <bansal2407@gmail.com>
2019-03-10 00:38:59 +0530

    Create collectTweets.py

---
+++ collectTweets.py
@@ -0,0 +1,16 @@
+from twython import Twython
+
+APP_KEY = "put api key here"
+APP_SECRET = "put api secret here"
+OAUTH_TOKEN = "put access token here"
+OAUTH_TOKEN_SECRET = "put access token secret here" 
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET) 
+
+f = open('collected_data.txt','w')
+
+username=raw_input("Enter username: ")
+user_timeline  = twitter.get_user_timeline(screen_name=username, count = 1000)
+for tweet in user_timeline:
+	print >>f, tweet['text'].encode('utf-8')
+	f.write("\n")
